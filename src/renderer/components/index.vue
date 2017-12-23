1ace7ce12795164aafc8197075f3bdc21981dc61
sa2taka <sa2taka@gmail.com>
2017-12-23 14:56:51 +0900

    Add index page(this is test page)

---
+++ src/renderer/components/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="main">
+      <h1>Hello electron-vue. My name is {{ name }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'sa2taka'
+    }
+  },
+  name: 'index'
+}
+</script>
+
+<style>
+html, body, main {
+  height: 100%;
+}
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
