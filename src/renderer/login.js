79641b423cfa663fecb3eb45fd693fbe886bdb20
sa2taka <sa2taka@gmail.com>
2017-12-24 00:56:03 +0900

    Change few specification

--- src/renderer/login.js
+++ src/renderer/login.js
@@ -1,4 +1,5 @@
 import Mastodon from '../../node_modules/mastodon-api/lib/mastodon'
+import Vue from 'vue'
 
 export default {
   data () {
@@ -64,51 +65,66 @@ export default {
     },
     showLogin (url, clientId, clientSecret, baseUrl) {
       console.log(url)
-      let electron = require('electron')
-      let BrowserWindow = electron.remote.BrowserWindow
+      let Electron = require('electron')
+      let BrowserWindow = Electron.remote.BrowserWindow
       let loginWindow = new BrowserWindow({ width: 800, height: 600 })
 
-      loginWindow.loadURL(url)
-      loginWindow.show()
+      Electron.remote.session.defaultSession.clearStorageData(function () { // ストレージデータを削除
+        loginWindow.loadURL(url)
+        loginWindow.show()
 
-      loginWindow.webContents.on('will-navigate', (event, url) => {
-        loginWindow.close()
-        console.log(url)
-        let matched = url.match(/\/authorize\/(.*)/)
-        console.log(matched)
-        if (matched) {
-          let self = this
-          Mastodon.getAccessToken(clientId, clientSecret, matched[1], baseUrl)
-            .catch(err => console.error(err))
-            .then(accessToken => {
-              console.log(accessToken)
-              const MstdnClient = new Mastodon({
-                access_token: accessToken,
-                api_url: baseUrl + '/api/v1/'
-              })
-              MstdnClient.get('accounts/verify_credentials', function (err, data, res) {
-                if (!err) {
-                  console.log(data)
-                  console.log(res)
-                  var id = data.id
-                  const doc = {
-                    url: baseUrl,
-                    account_id: id,
-                    type: 'account'
-                  }
-                  let db = self.$db
-                  db.insert(doc, function (err) {
-                    if (err) {
-                      console.log(err.stack)
-                    }
-                    db.find({type: 'account'}, (_, docs) => {
-                      console.dir(docs)
+        loginWindow.webContents.on('will-navigate', (event, url) => {
+          loginWindow.close()
+          console.log(url)
+          let matched = url.match(/\/authorize\/(.*)/)
+          console.log(matched)
+          if (matched) {
+            let self = this
+            Mastodon.getAccessToken(clientId, clientSecret, matched[1], baseUrl)
+              .catch(err => console.error(err))
+              .then(accessToken => {
+                console.log(accessToken)
+                const MstdnClient = new Mastodon({
+                  access_token: accessToken,
+                  api_url: baseUrl + '/api/v1/'
+                })
+
+                Vue.prototype.$client = MstdnClient
+                MstdnClient.get('accounts/verify_credentials', function (err, data, res) {
+                  if (!err) {
+                    console.log(data)
+                    console.log(res)
+                    var id = data.id
+                    let db = self.$db
+                    db.find({ url: baseUrl, id: id }, (err, data)).exec(function (err, data) {
+                      console.log('register')
+                      if (err) {
+                        console.log(err)
+                        return
+                      }
+                      if (data.length === 0) {
+                        const doc = {
+                          url: baseUrl,
+                          account_id: id,
+                          type: 'account',
+                          is_active: true
+                        }
+
+                        db.insert(doc, function (err) {
+                          if (err) {
+                            console.log(err.stack)
+                          }
+                          db.find({type: 'account'}, (_, docs) => {
+                            console.dir(docs)
+                          })
+                        })
+                      }
                     })
-                  })
-                }
+                  }
+                })
               })
-            })
-        }
+          }
+        })
       })
     }
   }
