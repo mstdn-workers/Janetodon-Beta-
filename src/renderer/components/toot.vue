2b74b147f4e43e7b4f0156b6c9efdbda7e608851
sa2taka <sa2taka@gmail.com>
2017-12-24 15:25:17 +0900

    Add to show toot length

--- src/renderer/components/toot.vue
+++ src/renderer/components/toot.vue
@@ -7,6 +7,14 @@
       <b-field>
         <b-input type="textarea" placeholder="本文(Ctrl-enterで送信)" v-model="mainText" @keyup.native.ctrl.enter="toot"></b-input>
       </b-field>
+      <div class="right">
+        <div class="red" v-if="tootLength > 500">
+          {{ 500 - tootLength }} / 500
+        </div>
+        <div v-else>
+          {{ 500 - tootLength }} / 500
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -39,6 +47,11 @@ export default {
       })
     }
   },
+  computed: {
+    tootLength: function () {
+      return this.mainText.length + this.spoilerText.length
+    }
+  },
   name: 'toot'
 }
 </script>
@@ -46,5 +59,11 @@ export default {
 <style>
 html, body, main {
   height: 100%;
+}
+.right {
+  text-align: right;
+}
+.red {
+  color: red;
 }
 </style>
