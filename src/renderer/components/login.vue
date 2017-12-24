4ea64bebacb123d96438c255b8bf8ac317ccecc2
sa2taka <sa2taka@gmail.com>
2017-12-24 12:37:52 +0900

    Add formate url system

--- src/renderer/components/login.vue
+++ src/renderer/components/login.vue
@@ -3,7 +3,7 @@
     <div class="main">
       <h1>ログインしたいインスタンスを入力してください</h1>
       <div class="error" v-if="error !== ''"> {{ error }} </div>
-      <input id="instance" type="text" placeholder="https://mstdn-workers.com" v-model="baseUrl"> </input>
+      <input id="instance" type="text" placeholder="mstdn-workers.com" v-model="baseUrl"> </input>
       <button @click="loginAndNext">次へ</button>
     </div>
   </div>
@@ -19,9 +19,23 @@ export default {
       baseUrl: ''
     }
   },
+  computed: {
+    formatedUrl: function () {
+      let replaced = this.baseUrl
+      if (replaced === '') {
+        replaced = 'https://mstdn-workers.com'
+      }
+      replaced.replace(/\/+$/, '')
+      replaced.replace(/^http:\/\//, 'https://')
+      if (!replaced.match(/^https:\/\//)) {
+        replaced = 'https://' + replaced
+      }
+      return replaced
+    }
+  },
   methods: {
     loginAndNext () {
-      this.login(this.baseUrl)
+      this.login(this.formatedUrl)
     }
   },
   name: 'login'
