01e4bb3ffe064046bb981e97745cb396c84202e5
sa2taka <sa2taka@gmail.com>
2017-12-24 13:18:24 +0900

    Add systen which auto jump to main-content when going to index

--- src/renderer/components/index.vue
+++ src/renderer/components/index.vue
@@ -8,11 +8,31 @@
 </template>
 
 <script>
+import Mastodon from '../../../node_modules/mastodon-api/lib/mastodon'
+import Vue from 'vue'
+
 export default {
   data () {
     return {
       name: 'sa2taka'
     }
+  },
+  beforeCreate () {
+    let self = this
+    this.$db.find({ type: 'account', is_active: true }).exec(function (err, data) {
+      if (err) {
+        console.log(err)
+        return
+      }
+      if (data.length !== 0) {
+        Vue.prototype.$client = new Mastodon({
+          access_token: data[0].access_token,
+          api_url: data[0].url
+        })
+        console.log(self.$client)
+        self.$router.push({ name: 'main-content' })
+      }
+    })
   },
   name: 'index'
 }
