7787c87a21a4723d22b2e774abb4daeab1163065
sa2taka <sa2taka@gmail.com>
2017-12-24 16:03:53 +0900

    Add avatar in status

--- src/renderer/components/one_status.vue
+++ src/renderer/components/one_status.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="status">
-    <figure class="image is-1by1 status_avatar">
-      <img src="https://mstdn-workers.s3-ap-northeast-1.amazonaws.com/accounts/avatars/000/000/424/original/64e8750285871421301a27f070462c45.gif" />
-    </figure>
+  <div class="status">
+    <div class="status_avatar">
+      <div class="account_avatar" style="width: 48px; height: 48px;background-size: 48px 48px; background-image: url('https://mstdn-workers.s3-ap-northeast-1.amazonaws.com/accounts/avatars/000/000/424/original/64e8750285871421301a27f070462c45.gif');">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,11 +22,30 @@ export default {
 html, body, main {
   height: 100%;
 }
+.status {
+  padding: 8px 10px;
+  padding-left: 68px;
+  position: relative;
+  min-height: 48px;
+  border-bottom: 1px solid #393f4f;
+  cursor: default;
+  opacity: 1;
+  -webkit-animation: fade .15s linear;
+  animation: fade .15s linear;
+}
 .status_avatar {
   width: 48px;
   height: 48px;
   left: 10px;
   position: absolute;
   top: 10px;
+}
+.account_avatar {
+  border-radius: 4px;
+  background: transparent no-repeat;
+  background-position: 50%;
+  background-clip: padding-box;
+  position: relative;
+  cursor: pointer;
 }
 </style>
