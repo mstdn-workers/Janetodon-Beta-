ebe3d950284245a56cf30a7dba4a74eef223f97a
sa2taka <sa2taka@gmail.com>
2017-12-25 17:20:55 +0900

    Change to divide image-update-area from toot.vue

---
+++ src/renderer/components/image_upload_area.vue
@@ -0,0 +1,177 @@
+<template>
+  <div>
+    <div v-if="isFileEnter && dropMedia.length <= 4" class="view-box-active">
+      <b-upload v-model="dropMedia" multiple drag-drop>
+        <div class="view-box"></div>
+      </b-upload>
+      <div class="upload-area">
+        <div class="upload-area-content">
+          <div class="center">
+            ドラッグ&amp;ドロップでアップロード
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else class="view-box-deleted">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isFileEnter: {},
+    dropMedia: {}
+  },
+  data () {
+    return {
+    }
+  },
+  name: 'image-upload-area'
+}
+</script>
+
+<style>
+html, body, main {
+  height: 100%;
+}
+.view-box {
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.0);
+  display: flex;
+  z-index: 500;
+  /* visibility: hidden; */
+  position: fixed;
+  top: 0px;
+  left: 0px;
+
+  display: flex;
+}
+
+.view-box-active {
+  animation-name: fadeIn;
+  animation-duration: 300ms;
+  animation-timing-function: ease;
+
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.67);
+
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  z-index: 500;
+
+  /* 左右中央 */
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+
+  /* 上下中央 */
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+
+  display: flex;
+}
+
+.view-box-deleted {
+  animation-name: fadeOut;
+  animation-duration: 200ms;
+  animation-timing-function: ease;
+
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.67);
+  opacity: 0;
+
+  z-index: 500;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+
+  pointer-events: none;
+}
+
+.upload-area {
+  border-radius: 8px;
+
+  width: 320px;
+  height: 160px;
+
+  display: flex;
+
+  box-sizing: border-box;
+  position: relative;
+
+  background-color: rgb(72, 68, 87);
+
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+
+  /* 上下中央 */
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
+
+.upload-area-content {
+  /* 左右中央 */
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+
+  /* 上下中央 */
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+
+  flex: 1;
+  color: rgb(215, 215, 215);
+  font-size: 20px;
+  font-weight: 600;
+
+  border: 2px dotted;
+
+  height: 90%;
+  border-radius: 8px;
+}
+
+.center {
+  height: 100%;
+  display: flex;
+  /* 左右中央 */
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+
+  /* 上下中央 */
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
+
+@@keyframes fadeIn {
+  0% {
+    opacity: 0%;
+  }
+  100% {
+    opacity: 100%;
+  }
+}
+
+@@keyframes fadeOut {
+  0% {
+    opacity: 100%;
+  }
+  100% {
+    opacity: 0%
+  }
+}
+
+</style>
--- src/renderer/components/toot.vue
+++ src/renderer/components/toot.vue
@@ -6,23 +6,6 @@
     </b-field>
 
 
-    <div v-if="isFileEnter && dropMedia.length <= 4" class="view-box-active">
-      <b-upload v-model="dropMedia" multiple drag-drop>
-        <div class="view-box"></div>
-      </b-upload>
-      <div class="upload-area">
-        <div class="upload-area-content">
-          <div class="center">
-            ドラッグ&amp;ドロップでアップロード
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div v-else class="view-box-deleted">
-
-    </div>
-
     <b-field>
       <b-input type="textarea" placeholder="本文(Ctrl-enterで送信)" v-model="mainText" @keyup.native.ctrl.enter="toot"></b-input>
     </b-field>
@@ -68,6 +51,7 @@
 
 <script>
 import TootVisibility from '@/components/toot_visibility'
+import ImageUploadArea from '@/components/image_upload_area'
 
 export default {
   props: {
@@ -96,7 +80,6 @@ export default {
           console.log(err)
         }
         console.log(data)
-        console.log(res)
       })
     },
     encodePath (file) {
@@ -109,7 +92,8 @@ export default {
     }
   },
   components: {
-    TootVisibility
+    TootVisibility,
+    ImageUploadArea
   },
   name: 'toot'
 }
@@ -158,128 +142,6 @@ html, body, main {
   color: rgb(41, 208, 183);
 }
 
-.view-box {
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.0);
-  display: flex;
-  z-index: 500;
-  /* visibility: hidden; */
-  position: fixed;
-  top: 0px;
-  left: 0px;
-
-  display: flex;
-}
-
-.view-box-active {
-  animation-name: fadeIn;
-  animation-duration: 300ms;
-  animation-timing-function: ease;
-
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.67);
-
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  position: fixed;
-  top: 0px;
-  left: 0px;
-  z-index: 500;
-
-  /* 左右中央 */
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  justify-content: center;
-
-  /* 上下中央 */
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  align-items: center;
-
-  display: flex;
-}
-
-.view-box-deleted {
-  animation-name: fadeOut;
-  animation-duration: 200ms;
-  animation-timing-function: ease;
-
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.67);
-  opacity: 0;
-
-  z-index: 500;
-  position: fixed;
-  top: 0px;
-  left: 0px;
-
-
-  pointer-events: none;
-}
-
-.upload-area {
-  border-radius: 8px;
-
-  width: 320px;
-  height: 160px;
-
-  display: flex;
-
-  box-sizing: border-box;
-  position: relative;
-
-  background-color: rgb(72, 68, 87);
-
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  justify-content: center;
-
-  /* 上下中央 */
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  align-items: center;
-}
-
-.upload-area-content {
-  /* 左右中央 */
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  justify-content: center;
-
-  /* 上下中央 */
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  align-items: center;
-
-  flex: 1;
-  color: rgb(215, 215, 215);
-  font-size: 20px;
-  font-weight: 600;
-
-  border: 2px dotted;
-
-  height: 90%;
-  border-radius: 8px;
-}
-
-.center {
-  height: 100%;
-  display: flex;
-  /* 左右中央 */
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  justify-content: center;
-
-  /* 上下中央 */
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  align-items: center;
-}
-
 .upload-media-gallery{
     height: 128px;
     width: 512px;
@@ -297,25 +159,6 @@ html, body, main {
   -o-object-fit: cover;
   object-fit: cover;
 }
-
-@@keyframes fadeIn {
-  0% {
-    opacity: 0%;
-  }
-  100% {
-    opacity: 100%;
-  }
-}
-
-@@keyframes fadeOut {
-  0% {
-    opacity: 100%;
-  }
-  100% {
-    opacity: 0%
-  }
-}
-
 
 @keyframes verticalFadeIn {
   0% {
