0ad147b095a9327abf6953c8466a89b379e36d9b
sa2taka <sa2taka@gmail.com>
2017-12-30 00:58:35 +0900

    Setup my_icon vue file

---
+++ src/renderer/components/Toot/my_icon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my-icon">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  name: 'my_icon'
+}
+</script>
+
+<style lang="sass">
+</style>
