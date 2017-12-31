56ede6702283c97d7c6ef92d23236f4f81b55ee5
sa2taka <sa2taka@gmail.com>
2017-12-31 13:10:52 +0900

    0;276;0cAdd sidebar

--- src/renderer/components/mainContent.vue
+++ src/renderer/components/mainContent.vue
@@ -1,8 +1,14 @@
 <template>
   <!-- undefinedに!!をつけるとfalseに他はtrueになる -->
   <div class="main" @dragenter="isFileEnter=true" @dragover="isFileEnter=true" @dragleave="isFileEnter=false"  @drop="isFileEnter=false">
-    <toot :isFileEnter="isFileEnter" @media-change="onMediaChange" @spoiler-change="onSpoilerChange"></toot>
-    <timeline :isMediaExist="isMediaExist" :isSpoilerActive="isSpoilerActive"></timeline>
+    <div class="side-bar">
+
+    </div>
+
+    <div class="content">
+      <toot :isFileEnter="isFileEnter" @media-change="onMediaChange" @spoiler-change="onSpoilerChange"></toot>
+      <timeline :isMediaExist="isMediaExist" :isSpoilerActive="isSpoilerActive"></timeline>
+    </div>
   </div>
 </template>
 
@@ -42,6 +48,23 @@ export default {
 <style lang="sass">
 .main
   display: flex
+
+.content
+  display: flex
+  position: relative
+  width: 100%
+
+  left: 64px
+
+.side-bar
+  width: 64px
+  height: 100%
+
+  background-color: rgb(24, 28, 42)
+
+  box-shadow: inset 0 10px 10px rgba(0, 0, 0, 0.5)
+
+  position: fixed
 
 .icon-image
   border-radius: 24px
