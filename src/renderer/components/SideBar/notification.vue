15281025dd7ac77cef80645021459af1cec67c57
sa2taka <sa2taka@gmail.com>
2017-12-31 22:37:48 +0900

    Add to display notification timeline

--- src/renderer/components/SideBar/notification.vue
+++ src/renderer/components/SideBar/notification.vue
@@ -3,49 +3,63 @@
     <a :class="{ 'notifications_button': true, 'notifications_active': isSelecting}">
       <b-icon icon="bell" size="is-large"></b-icon>
     </a>
+    <div :class="['notifications_timeline', isSelecting ? 'notifications_timeline_active' : 'notifications_timeline_delete']">
+      <notification-status :notification="notification" v-for="notification in reverseNotifications"></notification-status>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        isSelecting: false,
-        notifications: []
+import notificationStatus from '@/components/SideBar/notification_status'
+
+export default {
+  data () {
+    return {
+      isSelecting: false,
+      notifications: []
+    }
+  },
+  mounted () {
+    let self = this
+    this.getNotifications()
+
+    const listener = this.$client.stream('streaming/user')
+
+    listener.on('message', msg => {
+      if (msg.event === 'notification') {
+        self.notification = self.notifications.concat(msg.data)
       }
-    },
-    mounted () {
+    })
+
+    window.addEventListener('click', function (event) {
+      if (event.target.parentNode.parentNode.className === 'notifications_button') {
+        self.isSelecting = !self.isSelecting
+      } else {
+        self.isSelecting = false
+      }
+    })
+  },
+  methods: {
+    getNotifications () {
       let self = this
-      this.getNotifications()
 
-      const listener = this.$client.stream('streaming/user')
-
-      listener.on('message', msg => {
-        if (msg.event === 'notification') {
-          self.notification = self.notifications.concat(msg.data)
-        }
-      })
-
-      window.addEventListener('click', function (event) {
-        if (event.target.parentNode.parentNode.className === 'notifications_button') {
-          self.isSelecting = !self.isSelecting
-        } else {
-          self.isSelecting = false
-        }
-      })
-    },
-    methods: {
-      getNotifications () {
-        let self = this
-
-        this.$client.get('notifications', {})
-          .then(resp => {
-            self.notifications = self.notifications.concat(resp.data)
-          })
-      }
-    },
-    name: 'notification'
-  }
+      this.$client.get('notifications', {})
+        .then(resp => {
+          self.notifications = self.notifications.concat(resp.data)
+        })
+    }
+  },
+  computed: {
+    reverseNotifications: function () {
+      let reverseNotifications = this.notifications.slice() // 引数無しで配列の値渡しができる
+      return reverseNotifications.reverse()
+    }
+  },
+  components: {
+    notificationStatus
+  },
+  name: 'notification'
+}
 </script>
 
 <style lang="sass">
@@ -55,12 +69,42 @@ html body
   width: 64px
   height: 64px
   &_active
-    color: rgb(232, 213, 45)!important
+    color: rgb(215, 209, 50)!important
     &:hover
-      color: rgb(232, 213, 45)!important
+      color: rgb(215, 209, 50)!important
   &_button
     background-color: $side-bar-back!important
     &:hover
       background-color: $side-bar-back!important
+  &_timeline
+    position: absolute
+    transition: all 200ms 0s ease
+    display: flex
+    flex-flow: column-reverse
+    border: solid 5px white
+    border-radius: 10px
+    background-color: $main-background
+    width: 80vw
+    max-height: 80vh
+    top: 0px
+    left: 64px
+    transform-origin: -25px 90px
 
+    &:after
+      content: ""
+      display: block
+      position: absolute
+      width: 0
+      height: 0
+      border-style: solid
+      border-width: 0 10px 20px
+      border-color: transparent transparent rgb(102, 96, 135)
+      top: 80px
+      left: -25px
+      transform: rotate(-90deg)
+
+    &_active
+
+    &_delete
+      transform: scale(0)
 </style>
---
+++ src/renderer/components/SideBar/notification_status.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="notification-status">
+    <div class="status-info">
+      <div class="status-avatar">
+        <div class="account-avatar icon-image" :style="avatarStyle">
+        </div>
+      </div>
+      <span class="display-name">
+        {{ status.account.display_name }}
+        <span class="display-username">{{ '@' + status.account.acct }}</span>
+      </span>
+    </div>
+    <div class="status-content">
+      <div v-if="!status.spoiler_text">
+        <span v-html="content"></span>
+      </div>
+      <div v-else>
+        <span v-html="spoilerText"></span>
+        <b-collapse :open="false">
+          <button class="button is-small" slot="trigger" @click="isVisible = ~isVisible">
+            {{ isVisible ? "隠す" : " 続きを見る" }}
+          </button>
+          <div>
+            <span v-html="content"></span>
+          </div>
+        </b-collapse>
+      </div>
+    </div>
+
+    <action-bar :status="status"></action-bar>
+
+    <image-gallery v-if="status.media_attachments.length !== 0" :media="status.media_attachments" :sensitive="status.sensitive"></image-gallery>
+  </div>
+</template>
+
+<script>
+import ImageGallery from '@/components/Timeline/image_gallery'
+import ActionBar from '@/components/Timeline/action_bar'
+
+export default {
+  props: {
+    notification: {}
+  },
+  data () {
+    return {
+      status: {},
+      isVisible: false
+    }
+  },
+  methods: {
+    convertUrlToLink (str) {
+      return str.replace(/https?:\/\/([^\s<>]*)/g, function () {
+        return '<a href="' + arguments[0] + '" target="_blank">' + arguments[1].slice(0, 30) + '...' + '</a>'
+      })
+    }
+  },
+  computed: {
+    avatarStyle: function () {
+      console.log(this.status)
+      return "width): 48px; height: 48px;background-size: 48px 48px; background-image: url('" + this.status.account.avatar + "');"
+    },
+    spoilerText: function () {
+      let displayText = this.status.spoiler_text.replace(/<(?!br)(.|\s).*?>/g, '')
+      return this.convertUrlToLink(displayText)
+    },
+    content: function () {
+      let displayText = this.status.content.replace(/<(?!br)(.|\s).*?>/g, '')
+      return this.convertUrlToLink(displayText)
+    }
+  },
+  beforeMount () {
+    this.status = this.notification.status
+  },
+  components: {
+    ImageGallery,
+    ActionBar
+  },
+  name: 'notification-status'
+}
+</script>
+
+<style lang="sass">
+$action-active-color: rgb(25, 155, 179)
+.notification-status
+  padding: 8px 10px
+  padding-left: 68px
+  position: relative
+  min-height: 48px
+  border-bottom: 1px solid rgb(44, 71, 112)
+  cursor: default
+  font-size: 15px
+  border-radius: 8px
+  background-color:
+
+.status-avatar
+  width: 48px
+  height: 48px
+  left: 10px
+  position: absolute
+  top: 10px
+
+.account-avatar
+  background: transparent no-repeat
+  background-position: 50%
+  background-clip: padding-box
+  position: relative
+  cursor: pointer
+
+.display-name
+  display: block
+  max-width: 100%
+  overflow: hidden
+  text-overflow: ellipsis
+  white-space: nowrap
+
+.display-username
+  font-size: 14px
+  color: gray
+
+.status-content
+  font-size: 15px
+  line-height: 20px
+  word-wrap: break-word
+  font-weight: 400
+  overflow: hidden
+  white-space: pre-wrap
+</style>
