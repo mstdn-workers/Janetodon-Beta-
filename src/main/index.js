ffa8b31d0f3e104815aa9dc8f30f0580803038e5
sa2taka <sa2taka@gmail.com>
2018-01-15 22:13:22 +0900

    Add window remember system

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import { app, BrowserWindow } from 'electron'
+import path from 'path'
 
 /**
  * Set `__static` path to static files in production
@@ -19,16 +20,40 @@ function createWindow () {
   /**
    * Initial window options
    */
+  var infoPath = path.join(app.getPath('userData'), 'bounds-info.json')
+  var fs = require('fs')
+
+  var boundsInfo
+  try {
+    boundsInfo = JSON.parse(fs.readFileSync(infoPath, 'utf8'))
+  } catch (e) {
+    boundsInfo = { width: 600, height: 800 }
+  }
+
   mainWindow = new BrowserWindow({
-    height: 563,
-    useContentSize: true,
-    width: 1000
+    width: boundsInfo.width,
+    height: boundsInfo.height,
+    useContentSize: true
   })
 
   mainWindow.loadURL(winURL)
 
+  mainWindow.on('close', () => {
+    var infoPath = path.join(app.getPath('userData'), 'bounds-info.json')
+
+    fs.writeFileSync(infoPath, JSON.stringify(mainWindow.getBounds()))
+  })
+
   mainWindow.on('closed', () => {
     mainWindow = null
+  })
+
+  mainWindow.on('resize', () => {
+    let bounds = mainWindow.getBounds()
+    if (bounds.width < 500) {
+      bounds.width = 500
+      mainWindow.setBounds(bounds)
+    }
   })
 }
 
@@ -36,7 +61,6 @@ app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    console.log('close')
     app.quit()
   }
 })
