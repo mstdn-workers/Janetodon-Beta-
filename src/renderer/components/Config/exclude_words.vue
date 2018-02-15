64fdbf3aff476d63443f91f1e65024f49caca2c6
sa2taka <sa2taka@gmail.com>
2018-02-16 00:43:52 +0900

    Add exclude_words.vue

---
+++ src/renderer/components/Config/exclude_words.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="config-accounts">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  name: 'exclude-words'
+}
+</script>
+
+<style lang="sass">
+</style>
