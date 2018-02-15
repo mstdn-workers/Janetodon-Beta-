75a2bb724bcdbcdff4fc985f0303ec02ca7da9a0
sa2taka <sa2taka@gmail.com>
2018-02-16 02:22:29 +0900

    Add to display exclude words

--- src/renderer/components/Config/exclude_words.vue
+++ src/renderer/components/Config/exclude_words.vue
@@ -8,6 +8,14 @@
         <b-icon icon="plus-circle" size="is-medium"></b-icon>
       </a>
     </div>
+
+    <b-table
+      :data="excludeWords"
+      :columns="columns"
+      :checked-rows.sync="checkedRows"
+      checkable
+      class="words-list">
+    </b-table>
   </div>
 </template>
 
@@ -15,7 +23,15 @@
 export default {
   data () {
     return {
-      addedWord: ''
+      excludeWords: [],
+      addedWord: '',
+      columns: [
+        {
+          field: 'word',
+          label: 'Exclude Word'
+        }
+      ],
+      checkedRows: []
     }
   },
   methods: {
@@ -29,6 +45,10 @@ export default {
         type: 'exclude_word'
       }
 
+      this.excludeWords.push({ 'word': this.addedWord })
+
+      this.$eventCaller.$emit('change-exclude', this.excludeWords)
+
       let self = this
       this.$db.insert(doc, function (err) {
         if (err) {
@@ -40,6 +60,16 @@ export default {
       })
     }
   },
+  mounted () {
+    let self = this
+    this.$db.find({ type: 'exclude_word' }).exec(function (err, data) {
+      if (err) {
+        console.log(err)
+        return
+      }
+      self.excludeWords = data
+    })
+  },
   name: 'exclude-words'
 }
 </script>
@@ -47,7 +77,9 @@ export default {
 <style lang="sass">
 .config-accounts
   width: 100%
-  min-height: 100px
+  min-height: 120px
+  height: 600px
+
 .add-form
   display: flex
   justify-content: center
@@ -55,4 +87,12 @@ export default {
   width: 100%
   position: relative
   top: 40px
+.words-list
+  position: relative
+  top: 40px
+  height: 80%
+  overflow-y: auto
+
+  &::-webkit-scrollbar
+    display: none
 </style>
