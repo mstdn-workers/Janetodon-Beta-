0a804672c174524fc67b1abd6e65b9464f5b5259
sa2taka <sa2taka@gmail.com>
2018-02-16 01:13:42 +0900

    Add input to exclude words menu in config

--- src/renderer/components/Config/exclude_words.vue
+++ src/renderer/components/Config/exclude_words.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="config-accounts">
-
+    <div class="add-form">
+      <b-field style="width: 80%">
+        <b-input v-model="addedWord" @keyup.native.enter="addExcludeWord"></b-input>
+      </b-field>
+      <a @click="addExcludeWord">
+        <b-icon icon="plus-circle" size="is-medium"></b-icon>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -8,13 +15,29 @@
 export default {
   data () {
     return {
+      addedWord: ''
     }
   },
   methods: {
+    addExcludeWord () {
+      if (this.addedWord === '') {
+        return
+      }
+    }
   },
   name: 'exclude-words'
 }
 </script>
 
 <style lang="sass">
+.config-accounts
+  width: 100%
+  min-height: 100px
+.add-form
+  display: flex
+  justify-content: center
+  color: white
+  width: 100%
+  position: relative
+  top: 40px
 </style>
